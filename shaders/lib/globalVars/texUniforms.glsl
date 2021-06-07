63b5ce1c9372e50639da5aa24a17ede2344c285c
Eldeston <fremfredric@gmail.com>
2021-06-07 15:57:47 +0400

    -Found the bottleneck and now finalizing...

--- shaders/lib/globalVars/texUniforms.glsl
+++ shaders/lib/globalVars/texUniforms.glsl
@@ -3,20 +3,30 @@ uniform sampler2D depthtex0;
 
 // Albedo texture color 0
 uniform sampler2D gcolor;
+// Normal map buffer(rgb)
 uniform sampler2D colortex1;
+// Lightmap coord(rg) and subsurface scattering
 uniform sampler2D colortex2;
+// Metallic, emissive, roughness
 uniform sampler2D colortex3;
+// AO, cloud mask, alpha
 uniform sampler2D colortex4;
+
+// Reflections
 uniform sampler2D colortex5;
+// Accumulation buffer, and exposure
 uniform sampler2D colortex6;
+// Bloom
 uniform sampler2D colortex7;
 
 // Noise sample, r for blue noise, g for white noise, and b for cell noise
 uniform sampler2D noisetex;
 
-// Shadow color
-uniform sampler2D shadowcolor0;
+#if defined COMPOSITE || defined DEFERRED
+    // Shadow color
+    uniform sampler2D shadowcolor0;
 
-// Shadow texture
-uniform sampler2DShadow shadowtex0;
-uniform sampler2DShadow shadowtex1;
+    // Shadow texture
+    uniform sampler2DShadow shadowtex0;
+    uniform sampler2DShadow shadowtex1;
+#endif
